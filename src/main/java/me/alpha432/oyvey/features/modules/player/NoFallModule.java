80da4422b41a1a572a8c5d8fba23c1df07aff5eb
imuser32234 <mateicodrut.dumitru@gmail.com>
2026-03-19 17:17:13 -0500

    Update NoFallModule.java

--- src/main/java/me/alpha432/oyvey/features/modules/player/NoFallModule.java
+++ src/main/java/me/alpha432/oyvey/features/modules/player/NoFallModule.java
@@ -1,21 +1,10 @@
-package me.alpha432.oyvey.features.modules.player;
-
-import me.alpha432.oyvey.OyVey;
-import me.alpha432.oyvey.features.modules.Module;
-import net.minecraft.network.protocol.game.ServerboundMovePlayerPacket;
-
-public class NoFallModule extends Module {
-    public NoFallModule() {
-        super("NoFall", "Removes fall damage", Category.PLAYER);
-    }
-
-    @Override
-    public void onTick() {
-        if (!mc.player.onGround() && OyVey.positionManager.getFallDistance() > 3) {
-            boolean bl = mc.player.horizontalCollision;
-            ServerboundMovePlayerPacket.PosRot pakcet = new ServerboundMovePlayerPacket.PosRot(mc.player.getX(), mc.player.getY() + 0.000000001, mc.player.getZ(),
-                    mc.player.getYRot(), mc.player.getXRot(), false, bl);
-            mc.player.connection.send(pakcet);
-        }
-    }
-}
+onAbilityUse(player):
+    if cooldown active:
+        return
+    
+    target = getNearestEntity(player, range=4)
+    
+    if target exists:
+        damage(target, amount=4)
+        playEffect()
+        setCooldown(player, 2 seconds)
